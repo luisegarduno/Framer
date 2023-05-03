e46591d7f1dcc8e0a1c02170ac4cdffabe6dcd12
Levi-B4 <levi.butler4@gmail.com>
2023-05-03 13:18:44 -0400

    created basic flipbook template but cannot get local images to show

---
+++ client/src/components/Flipbook.jsx
@@ -0,0 +1,58 @@
+import * as React from 'react';
+
+export class Flipbook extends React.Component {
+    constructor() {
+        super()
+        
+        this.onClickForward = this.onClickForward.bind(this);
+        this.onClickBackward = this.onClickBack.bind(this);
+    
+        const img001 = require("./videoFrames/001.jpg");
+        const img002 = require("./videoFrames/002.jpg");
+        const img003 = require("./videoFrames/003.jpg");
+        const img004 = require("./videoFrames/004.jpg");
+    
+        this.state = {
+        index: 0,
+        imgList: [img001, img002, img003, img004]
+        }
+    }
+
+    onClickForward() {
+        if (this.state.index + 1 === this.state.imgList.length) {
+            this.setState({
+                index: 0
+        })
+        } else {
+            this.setState({
+                index: this.state.index + 1
+            })
+        }
+    }
+
+    onClickBack() {
+        if (this.state.index - 1 === -1) {
+            this.setState({
+                index: this.state.imgList.length - 1
+        })
+        } else {
+            this.setState({
+                index: this.state.index -1
+            })
+        }
+    }
+    
+    // This is a placeholder. Feel free to edit or remove this code :)
+    render() {
+        return (
+        <div className="video">
+            <main>
+                <img id="flipbook" src={this.state.imgList[this.state.index]} alt="Flipbook"/><br/>
+                <button onClick={this.onClickBackward}>Previous</button>
+                <button onClick={this.onClickForward}>Next</button>
+            </main>
+        </div>
+        );
+    }
+}
+export default Flipbook;
--- client/src/components/app.jsx
+++ client/src/components/app.jsx
@@ -1,15 +1,16 @@
 import * as React from 'react';
 import "./app.css";
+import Flipbook from "./Flipbook.jsx";
 
 // This is a placeholder. Feel free to edit or remove this code :)
 export function App() {
   return (
     <div className="app">
-      <header className="appHeader">
-        <h1>Digital flipbook player</h1>
+      <header>
+        <h1>Digital Flipbook Player</h1>
       </header>
       <main>
-        <p>Good luck!</p>
+        <Flipbook></Flipbook>
       </main>
     </div>
   );
