d0988332dcd13727bfdd4b9b99b24370346da516
Levi-B4 <levi.butler4@gmail.com>
2023-05-04 14:30:13 -0400

    Changed Flipbook class to functional component, and transferred image fetch to it

--- client/src/components/Flipbook.jsx
+++ client/src/components/Flipbook.jsx
@@ -1,58 +1,22 @@
 import * as React from 'react';
 
-export class Flipbook extends React.Component {
-    constructor() {
-        super()
-        
-        this.onClickForward = this.onClickForward.bind(this);
-        this.onClickBackward = this.onClickBack.bind(this);
+export default function Flipbook({ videoFolderUrl }) {
+    const [img, setImg] = React.useState();
     
-        const img001 = require("./videoFrames/001.jpg");
-        const img002 = require("./videoFrames/002.jpg");
-        const img003 = require("./videoFrames/003.jpg");
-        const img004 = require("./videoFrames/004.jpg");
-    
-        this.state = {
-        index: 0,
-        imgList: [img001, img002, img003, img004]
-        }
-    }
+    const fetchImage = async () => {
+        const folder = await fetch(videoFolderUrl);
+        const frame001 = folder + "001.jpg";
+        const imageBlob = await frame001.blob();
+        const imageObjectURL = URL.createObjectURL(imageBlob);
+        setImg(imageObjectURL);
+    };
 
-    onClickForward() {
-        if (this.state.index + 1 === this.state.imgList.length) {
-            this.setState({
-                index: 0
-        })
-        } else {
-            this.setState({
-                index: this.state.index + 1
-            })
-        }
-    }
+  
+    React.useEffect(() => {
+      fetchImage();
+    }, []);
 
-    onClickBack() {
-        if (this.state.index - 1 === -1) {
-            this.setState({
-                index: this.state.imgList.length - 1
-        })
-        } else {
-            this.setState({
-                index: this.state.index -1
-            })
-        }
-    }
-    
-    // This is a placeholder. Feel free to edit or remove this code :)
-    render() {
-        return (
-        <div className="video">
-            <main>
-                <img id="flipbook" src={require("./videoFrames/001.jpg")} alt="Flipbook"/><br/>
-                <button onClick={this.onClickBackward}>Previous</button>
-                <button onClick={this.onClickForward}>Next</button>
-            </main>
-        </div>
-        );
-    }
+    return(
+        <img src={img} alt="Flipbook"/>
+    )
 }
-export default Flipbook;
