76c6f9dc8455dd8257aaf1867e85765c3b5fbb11
Levi-B4 <levi.butler4@gmail.com>
2023-05-08 23:36:33 -0400

    moved frame handling to flipbook inside usestate hook

--- client/src/components/flipbook/flipbook.jsx
+++ client/src/components/flipbook/flipbook.jsx
@@ -1,26 +1,34 @@
 import * as React from 'react';
-import  requestFramePath from './requestFramePath';
-
 import "./flipbook.css";
-import { request } from 'http';
 
 const host = "http://localhost:3001/";
-const tempArray = ["001.jpg", "002.jpg", "003.jpg", "004.jpg", "005.jpg", "006.jpg", "007.jpg", "008.jpg", "009.jpg", "010.jpg"];
+const tempArray = ["001.jpg", "002.jpg", "003.jpg", "004.jpg", "005.jpg", "006.jpg", "007.jpg", "008.jpg", "009.jpg", "010.jpg"]; //temp replicate of api output
 
 export default function Flipbook({ videoFolderUrl }) {
     const [img, setImg] = React.useState();
+    let currentFrameNum = 0;
+    const fps = 30;
 
     //VideoRepo(videoFolderUrl).getVideo()
 
-    async function fetchFrame(frameName = 0){
-        const frame = await fetch(videoFolderUrl + frameName);
-        const imageBlob = await frame.blob();
-        const imageObjectURL = URL.createObjectURL(imageBlob);
+    async function fetchFrame(frameName = 0){                       //fetches frame from server from path
+        const frame = await fetch(videoFolderUrl + frameName);      
+        const imageBlob = await frame.blob();                       //converts frame to blob
+        const imageObjectURL = URL.createObjectURL(imageBlob);      //creates url for blob
         setImg(imageObjectURL);
     };
 
     React.useEffect(() => {
-        fetchFrame(requestFramePath(tempArray, 30));
+        //create interval to fetch next frame every 1/fps seconds
+        setInterval(() => {
+            if(currentFrameNum >= tempArray.length - 1){        //if at end of video, reset to beginning
+                currentFrameNum = 0;
+            }
+            else{                                               //else, fetch next frame
+                currentFrameNum++;
+                fetchFrame(tempArray[currentFrameNum])
+            }
+        }, 1000/fps);
     }, []);
 
     return(
